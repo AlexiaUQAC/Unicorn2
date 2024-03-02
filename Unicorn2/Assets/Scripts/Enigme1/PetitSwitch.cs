136397866ffcf91696af50f158a07b489ed1616b
Mathieu Dubois <mathieudubois403@gmail.com>
2024-03-02 12:38:30 -0500

    Petit switch

--- Unicorn2/Assets/Scripts/Enigme1/PetitSwitch.cs
+++ Unicorn2/Assets/Scripts/Enigme1/PetitSwitch.cs
@@ -9,7 +9,8 @@ public class PetitSwitch : MonoBehaviour
     //[SerializeField] private BoxCollider _boxCollider;
     [SerializeField] private bool _switch;
     [SerializeField] private GameObject _screen;
-    private string _lastPlayerInRange;
+    [SerializeField] private List<string> _playerInRange;
+    
 
     private Vector3 _screenOpen = new Vector3(1, 1, 1);
     private Vector3 _screenClose = new Vector3(0, 0, 0);
@@ -29,7 +30,7 @@ public class PetitSwitch : MonoBehaviour
     {
         _switch = false;
         _screen.transform.DOScale(_screenClose, 1);
-        _lastPlayerInRange = "";
+        _playerInRange = new List<string>();
     }
 
     /*private void OnTriggerStay(Collider other)
@@ -52,11 +53,13 @@ public class PetitSwitch : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         EventsManager.PlayerInActionSudRange(other, true, "<ACTIVER>");
+        _playerInRange.Add(other.tag);
     }
 
     private void OnTriggerExit(Collider other)
     {
         EventsManager.PlayerInActionSudRange(other, false, "");
+        _playerInRange.Remove(other.tag);
     }
 
     public bool GetSwitchStatus()
@@ -66,19 +69,22 @@ public class PetitSwitch : MonoBehaviour
 
     private void ActiverSwitch(string s)
     {
-        
-        _switch = !_switch;
-        if (_switch)
+        if(_playerInRange.Contains(s))
         {
-            _screen.transform.DOScale(_screenOpen, 1);
-            Debug.Log(name + "activé");
-        }
-        else
-        {
-            _screen.transform.DOScale(_screenClose, 1);
-            Debug.Log(name + "désactivé");
+            _switch = !_switch;
+            if (_switch)
+            {
+                _screen.transform.DOScale(_screenOpen, 1);
+                Debug.Log(name + "activé");
+            }
+            else
+            {
+                _screen.transform.DOScale(_screenClose, 1);
+                Debug.Log(name + "désactivé");
 
+            }
         }
+        
     }
 
 
